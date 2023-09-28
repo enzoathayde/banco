ef22692ac821a22ec59ef84c5027322ee1828aee
Enzo Athayde <enzo.athayde.dev@gmail.com>
2023-09-28 15:17:38 -0300

    inserção da função de saque e deposito

--- Main.java
+++ Main.java
@@ -4,12 +4,12 @@ public class Main
 {
   public static void main(String[] args)
   {
-    int alternativa = 12, qualbanco = 5;
+    int alternativa = 1, qualbanco = 1, qualdeposito = 1;
     Scanner ler = new Scanner(System.in);
     ContaPoupanca bancaria = new ContaPoupanca();
     ContaEspecial bancaria2 = new ContaEspecial();
-    bancaria.saldo = 5435;  //considere saldo em centavos
-    bancaria2.saldo = 3423; 
+    bancaria.saldo = 54;  //considere saldo em centavos
+    bancaria2.saldo = 34; 
     bancaria.nomeCliente = "Enzo Athayde";
     bancaria2.nomeCliente = "Lucas Padua";
     bancaria.limite = 300;
@@ -30,19 +30,27 @@ public class Main
         System.out.println("Caso 1: Sacar dinheiro");
         System.out.println("Caso 2: Mostrar nome do títular");
         System.out.println("Caso 3: Mostrar saldo da conta ");
+        System.out.println("Caso 4: Depositar dinheiro ");
         System.out.println("Caso 0: Encerrar o programa da conta poupança");
         System.out.println("Escolha:");
         alternativa = ler.nextInt();
           switch(alternativa)
           {
             case 1:
-            System.out.println("Você quer sacar dinheiro");
+            System.out.println("Informe o valor(limite):");
+            qualbanco = ler.nextInt();
+            bancaria.Sacar(qualbanco);
             break;
             case 2:
             System.out.printf("Nome: %s",bancaria.MostrarNome());
             break;
             case 3:
             System.out.printf("Saldo disponível: %d ", bancaria.MostrarSaldo());
+            break;
+            case 4:
+            System.out.println("Informe a quantidade a ser depositada");
+            qualdeposito = ler.nextInt();
+            bancaria.Depositar(qualdeposito);
             break;
             case 0:
             System.out.println("Você escolheu sair da conta poupança");
@@ -52,7 +60,7 @@ public class Main
             break;
           }
         }
-        alternativa = 12;
+        alternativa = 1;
         break;
         case 0:
         System.out.println("Você escolheu encerrar a aplicação");
@@ -64,13 +72,16 @@ public class Main
           System.out.println("Caso 1: Sacar dinheiro");
           System.out.println("Caso 2: Mostrar nome do títular");
           System.out.println("Caso 3: Mostrar saldo da conta ");
+          System.out.println("Caso 4: Depositar dinheiro ");
           System.out.println("Caso 0: Encerrar o programa da conta especial");
           System.out.println("Escolha:");
           alternativa = ler.nextInt();
             switch(alternativa)
             {
               case 1:
-              System.out.println("Você quer sacar dinheiro");
+              System.out.println("Informe o valor(limite):");
+              qualbanco = ler.nextInt();
+              bancaria2.Sacar(qualbanco);
               break;
               case 2:
               System.out.printf("Nome: %s",bancaria2.MostrarNome());
@@ -78,6 +89,10 @@ public class Main
               case 3:
               System.out.printf("Saldo disponível: %d ", bancaria2.MostrarSaldo());
               break;
+              case 4:
+              System.out.println("Informe a quantidade a ser depositada");
+              qualdeposito = ler.nextInt();
+              bancaria2.Depositar(qualdeposito);
               case 0:
               System.out.println("Você escolheu sair da conta poupança");
               break;
@@ -86,7 +101,7 @@ public class Main
               break;
             }
         }
-        alternativa = 12;
+        alternativa = 1;
         break;
       }
     }
